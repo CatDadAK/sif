1ee19c41ccbd719540525e2686caad5622aa83f8
Kasper Westeraa (KBW | AK) <AKKBW@katedralskolen.dk>
2026-03-25 09:43:22 +0100

    Main menu prototype

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void showMenu() {
+    system("cls");
+    printf("Velkommen til SiF\n");
+    printf("Hvad ønsker du at arbejde med?\n");
+    printf("1: Wien's forskydningslov\n");
+    printf("2: Hubble's lov\n");
+}
+
+
+int main() {
+    showMenu();
+}
