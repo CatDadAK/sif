97f8ff7a0ae6adbd51586f3b525fe8b7c045fbe2
Kasper Westeraa (KBW | AK) <AKKBW@katedralskolen.dk>
2026-04-09 07:39:42 +0200

    Frequency calculation added

--- main.c
+++ main.c
@@ -7,9 +7,32 @@ void showMenu() {
     printf("Hvad ønsker du at arbejde med?\n");
     printf("1: Wien's forskydningslov\n");
     printf("2: Hubble's lov\n");
+    printf("3: Frekvens fra bølgelængde og hastighed\n");
 }
 
+int getUserInput() {
+    int choice;
+    printf("Indtast dit valg: ");
+    scanf("%d", &choice);
+    return choice;
+}
+
+double calcFrequency(double wavelength, double speed) {
+    return speed / wavelength;
+}
 
 int main() {
     showMenu();
+    int choice = getUserInput();
+
+    if (choice == 3) {
+        double wavelength, speed;
+        printf("Indtast bølgelængde (m): ");
+        scanf("%lf", &wavelength);
+        printf("Indtast hastighed (m/s): ");
+        scanf("%lf", &speed);
+
+        double frequency = calcFrequency(wavelength, speed);
+        printf("Frekvens: %f Hz\n", frequency);
+    }
 }
